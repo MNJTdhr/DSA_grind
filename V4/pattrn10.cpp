db874957fccf1704f9983b0ba1dfb134f304122f
JackDS <manojit.dhar.jack@gmail.com>
2024-09-07 17:30:13 +0530

    v1

--- V4/pattrn10.cpp
+++ V4/pattrn10.cpp
@@ -5,26 +5,58 @@ using std::cin, std::cout;
 int main()
 {
     int n, k, l;
-
     cout << "Enter length: ";
+    // cout<<"4\n";
     cin >> n;
+    // n = 4;
 
     for (int i = 0; i < n; i++)
     {
         // space
-        for (int j = n; j > i; j--)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            cout << "<>";
+            cout << " ";
         }
+
+        cout << '*';
 
         // star
-        for (int j = n; j > i; j--)
+        if (i != 0)
         {
-            cout << "<>";
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+            cout << '*';
         }
+        cout << '\n';
+    }
 
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << '*';
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << ' ';
+            }
+            cout << '*';
+        }
         cout << '\n';
     }
 
     return 0;
 }
+
+//      *     
+//     * *    
+//    *   *   
+//   *     *  
+//    *   *   
+//     * *    
+//      *     
