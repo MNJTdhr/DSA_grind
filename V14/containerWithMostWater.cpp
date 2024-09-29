ac06b678669b6b2cba7a8e3cb6d90323374b39a7
JackDS <manojit.dhar.jack@gmail.com>
2024-09-29 22:18:18 +0530

    v14

---
+++ V14/containerWithMostWater.cpp
@@ -0,0 +1,32 @@
+// leetcode Question 11
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int MostWater(vector<int> arr);
+
+int main(){
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+    int size = sizeof(arr) / sizeof(int);
+    
+    int ans = MostWater(arr);
+    cout << "Most Water: " << ans;
+
+    return 0;
+}
+
+int MostWater(vector<int> arr){
+    int maxWater = 0, w, h, area = 1;
+    int start=0, end=arr.size()-1;
+
+    while(start<end){
+        w=end-start;
+        h=min(arr[start], arr[end]);
+        area=w*h;
+        maxWater=max(maxWater,area);
+
+        (arr[start]<arr[end]) ? start++ : end--;
+    }
+
+    return maxWater;
+}
---
+++ V15/contrWthMostWaterBrute.cpp
@@ -0,0 +1,33 @@
+// leetcode Question 11
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int MostWater(vector<int> arr);
+
+int main(){
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+    int size = sizeof(arr) / sizeof(int);
+    
+    int ans = MostWater(arr);
+    cout << "Most Water: " << ans;
+
+    return 0;
+}
+
+int MostWater(vector<int> arr){
+    int maxWater = 0, width = 0, height = 0, area = 1;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            height = min(arr[i], arr[j]);
+            width = j - i;
+            area = height * width;
+            maxWater = max(maxWater, area);
+        }
+    }
+
+    return maxWater;
+}
