9571a57439632eee5423aa320faa4e3abd77f10b
JackDS <manojit.dhar.jack@gmail.com>
2024-10-01 23:11:59 +0530

    better linear search

--- V8 array/linear_searcch.cpp
+++ V8 array/linear_searcch.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int size, int key);
-
-int main()
-{
-    int key;
-    int arr[] = {1, 2, 3, 6, 4, 7, 8, 11, 46, 116, 13, 32, 442, 36, 123, 6221, 44, 22, 332, 131};
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Enter key: ";
-    cin >> key;
-
-    int res = linearSearch(arr, size, key);
-
-    if (res == 1)
-    {
-        cout << "key found" << endl;
-    }
-    else
-        cout << "key not found" << endl;
-
-    return 0;
-}
-
-int linearSearch(int arr[], int size, int key)
-{
-    cout << "Total size of the array: " << size << endl;
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == key)
-        {
-            cout << "key found at posi: " << i + 1 << endl;
-            return 1;
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i;
         }
     }
     return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 0, 1, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 1;
+
+    int result = linearSearch(arr, size, key);
+    
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    return 0;
 }
