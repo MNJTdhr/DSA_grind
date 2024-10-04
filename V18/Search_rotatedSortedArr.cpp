92e9eef8f14163cf23cabd7491c9c7d203d61559
JackDS <manojit.dhar.jack@gmail.com>
2024-10-04 18:34:47 +0530

    v18

---
+++ V18/Search_rotatedSortedArr.cpp
@@ -0,0 +1,46 @@
+//leetcode Q33
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchRSA(vector<int> vec, int key){
+    int start=0, end=vec.size()-1;
+
+    while (start<=end){
+        int mid=start+(end-start)/2;
+        if (vec[mid]==key){
+            return mid;
+        }
+        if (vec[start]<=vec[mid]){
+            if (vec[start]<=key && key<=vec[mid]){
+                end=mid-1;
+            } else{
+                start=mid+1;
+            }
+        }
+        else{
+            if (vec[mid]<=key && key<=vec[end]){
+                start=mid+1;
+            } else{
+                end=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> vec={4,5,6,7,0,1,2};
+    int key=6;
+
+    int result=searchRSA(vec,key);
+
+    if (result!=-1)
+    {
+        cout<<"Key found at index: "<<result;
+    } else {
+        cout<<"Key not found";
+    }
+    
+    return 0;
+}
