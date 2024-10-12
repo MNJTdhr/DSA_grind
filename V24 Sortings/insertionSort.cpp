1ac1468c9babcedce1b4c456f3550dd661951e05
JackDS <manojit.dhar.jack@gmail.com>
2024-10-12 20:49:31 +0530

    insertion sort

--- V24 Sortings/insertionSort.cpp
+++ V24 Sortings/insertionSort.cpp
@@ -5,7 +5,6 @@ void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
-        // Move elements of arr[0..i-1], that are greater than key, to one position ahead
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
