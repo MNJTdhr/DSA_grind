d7ba86269b1169189d10a06384395ac675c941a6
JackDS <manojit.dhar.jack@gmail.com>
2024-10-25 23:13:52 +0530

    n

---
+++ V29 String valid Palindrom/strPalindromAlphaNum.cpp
@@ -0,0 +1,26 @@
+//leetcode q125
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int size = str.length();
+    for (int i = 0; i < size / 2; i++) {
+        if (str[i] != str[size - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string str="A man, a plan, a canal: Panama";
+
+    if (isPalindrome(str)) {
+        cout << str << " is a palindrome" << endl;
+    } else {
+        cout << str << " is not a palindrome" << endl;
+    }
+
+    return 0;
+}
