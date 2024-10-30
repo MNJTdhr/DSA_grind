c2698c9960aa9e64247953107a38a3790dbb6182
JackDS <manojit.dhar.jack@gmail.com>
2024-10-30 09:19:23 +0530

    v30

--- V30 Permutation in String/PermutationinString.cpp
+++ V30 Permutation in String/PermutationinString.cpp
@@ -1,22 +1,52 @@
-//leetcode q1910
+// leetcode q567
 #include <iostream>
 #include <string>
+#include <vector>
 
-void remvAllocc(std::string st, std::string subst){
-    while (st.length()>0 && st.find(subst)<st.length()) 
-    //original string must be greater than 0 in length AND
-    //finds the subst's length and confirms it's lesser in length then the larger string
-    {
-        st.erase(st.find(subst), subst.length());
+using namespace std;
+
+bool isFreqSame(const vector<int>& freq, const vector<int>& windFreq) {
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != windFreq[i]) {
+            return false;
+        }
     }
-    std::cout<<st;
+    return true;
 }
 
-int main(){
-    std::string st="daabcbaabcbc";
-    std::string subst="abc";
+bool isPermutationInString(string keyArr, string subjeckArr) {
+    if (keyArr.length() > subjeckArr.length()) return false;
 
-    remvAllocc(st,subst);
+    vector<int> freq(26, 0);    //26 spaces full of 0
+    for (char c : keyArr) {
+        freq[c - 'a']++;
+        // int indx=keyArr[i]-'a'; indx++; 
+    }
+
+    vector<int> windFreq(26, 0);
+    for (int i = 0; i < subjeckArr.length(); i++) {
+        windFreq[subjeckArr[i] - 'a']++;
+
+        if (i >= keyArr.length()) {
+            windFreq[subjeckArr[i - keyArr.length()] - 'a']--;
+        }
+
+        if (isFreqSame(freq, windFreq)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    string subjeckArr = "eidbaooo";
+    string keyArr = "ab";
+
+    if (isPermutationInString(keyArr, subjeckArr)) {
+        cout << "Present" << endl;
+    } else {
+        cout << "Absent" << endl;
+    }
 
     return 0;
 }
