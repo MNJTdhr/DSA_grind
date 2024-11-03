d0c9434cc6464b0721d6e96181ec22094cb2b1f0
JackDS <manojit.dhar.jack@gmail.com>
2024-11-03 23:12:38 +0530

    priority queue

---
+++ V27 Standard Template library/priorityQueue1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    priority_queue<int, vector<int>, greater<int>> p_qu;   // min-heap
+    p_qu.push(1);
+    p_qu.push(3);
+    p_qu.push(2);
+    p_qu.push(0);
+    cout<<"Current front value: "<<p_qu.top()<<endl;    // 0
+
+    cout<<"Whole priorityqueue: "<<endl;    // 0 1 2 3
+    while (!p_qu.empty()){
+        cout<<p_qu.top()<<' ';
+        p_qu.pop();
+    }
+    
+    return 0;
+}
+
