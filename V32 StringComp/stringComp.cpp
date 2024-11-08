6df7a73ee2abfd47150d7dcacd7a7e78e9493ea1
JackDS <manojit.dhar.jack@gmail.com>
2024-11-08 20:14:01 +0530

    v32

---
+++ V32 StringComp/stringComp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int stringCompress(string s) {
+    int size = s.size();
+    int idx = 0;
+    for (int i = 0; i < size; i++) {
+        char ch = s[i];
+        int count = 0;
+        while (i < size && s[i] == ch) {
+            count++;
+            i++;
+        }
+        if (count == 1) {
+            s[idx++] = ch;
+        } else {
+            s[idx++] = ch;
+            string str = to_string(count);
+            for (char dig : str) {
+                s[idx++] = dig;
+            }
+        }
+        i--;
+    }
+    s.resize(idx);
+    for (int i = 0; i < size; i++) {
+        cout << s[i] << '\t';
+    }
+    cout << endl;
+    return idx;
+}
+
+int main() {
+    vector<char> chhar = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    string s(chhar.begin(), chhar.end());
+    int idx = stringCompress(s);
+    cout << "idx= " << idx << endl;
+    return 0;
+}
