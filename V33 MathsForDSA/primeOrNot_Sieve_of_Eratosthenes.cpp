73d14b9a5009ffb9b7e6b869b2ed0dd6c0df16b1
JackDS <manojit.dhar.jack@gmail.com>
2024-11-16 20:54:05 +0530

    v33 1

--- V33 MathsForDSA/primeOrNot_Sieve_of_Eratosthenes.cpp
+++ V33 MathsForDSA/primeOrNot_Sieve_of_Eratosthenes.cpp
@@ -1,14 +1,26 @@
 //leetcode q204
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void isPrime(int num){
-    
+int countPrimes(int n){
+    vector<bool> isPrime(n+1, true);
+    int count =0;
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            count++;
+            for (int j = i+2; j<n; j=j+i)
+            {
+                isPrime[j]=false;
+            }            
+        }        
+    }
+    return count;
 }
 
-
-
 int main(){
-    
+    countPrimes(50);
     return 0;
 }
